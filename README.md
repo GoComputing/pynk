cfd30c663d97a7eb67ebee656c1617249b91bb09
Carlos Peláez González <carlosprog1997@gmail.com>
2022-04-04 02:03:52 +0200

    Added main skeleton to main source code and changed config format
    There are three different changes in this commit:
    
    * A new skeleton was implemented in the main source code (code.py).
      This skeleton reads the configuration and then creates an object
      of the new class Keyboard (defined in the same file). After the
      object is created, the loop function is called. In the future,
      this method will read keys status, send them to the computer
      and it will update the modules (for example: LEDs, displays and
      so on).
    
    * The configuration syntax is now JSON, as it is faster to read and
      CircuitPython includes libraries to read them so no dependencies
      are added.
    
    * The README.md now includes a list of dependencies and the version
      they were tested.

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # PYNK: Python Neon Keyboard firmware
 
 This project is intended to create a new firmware for custom mechanical
-keyboards, in a nimilar may as QMK or ZMK. Contrary to these firmwares,
-this one will be implemented using Python instead of C/C++. This is able
-to do thanks to CircuitPython, a firmware supported by several microcontrollers
-that reads and interpret Python code.
+keyboards, in a similar way as QMK or ZMK. Contrary to these firmwares,
+this one will be implemented using Python instead of C/C++. This is 
+being abled to do thanks to CircuitPython, a firmware supported by 
+several microcontrollers that reads and interpret Python code.
+
+
+## Dependencies
+
+There are some dependencies that we recommend they to be installed as
+frozen modules. Here is a list of the dependencies:
+
+| Name          | Tested version | URL                                               |
+|---------------|----------------|---------------------------------------------------|
+| CircuitPython | 7.2.3          | https://github.com/adafruit/circuitpython.git     |
+
--- src/code.py
+++ src/code.py
@@ -1,1 +1,98 @@
- 
+import json
+import time
+import os
+
+
+###############################
+# Parameters
+###############################
+
+CONFIG_PATH = "config.json"
+
+
+
+
+
+
+###############################
+# Helpers
+###############################
+
+# TODO
+def config_check(cfg, raise_except):
+    
+    # Config sintax
+    cfg_schema = {
+        'type': 'object',
+        'properties': {
+            'model' : { 'type' : 'string' },
+            'micro' : { 'type' : 'string' }
+        }
+    }
+
+def read_config(path):
+    
+    # Config read
+    with open(path, "r") as f:
+        raw_content = ''.join(f.readlines())
+    cfg = json.loads(raw_content)
+    
+    # Config syntax check
+    config_check(cfg, raise_except=True)
+    
+    return cfg
+
+
+
+
+
+
+###############################
+# Keyboard class
+###############################
+
+class Keyboard:
+    
+    def __init__(self, cfg):
+
+        self.model = cfg['model']
+        self.micro = cfg['micro']
+    
+    
+    def __str__(self):
+        
+        return 'Model ({}), Microcontroller ({})'.format(
+            self.model,
+            self.micro
+        )
+    
+    
+    def loop(self):
+        
+        while True:
+            time.sleep(0.1)
+
+
+
+
+
+
+###############################
+# Main
+###############################
+
+def main():
+    
+    # Read config
+    cfg = read_config(CONFIG_PATH)
+    
+    # Initialize keyboard and all its modules
+    keyboard = Keyboard(cfg)
+    print(keyboard)
+
+    # Keyboard loop
+    keyboard.loop()
+
+
+if __name__ == '__main__':
+    main()
